441557d86edb3e169fde6e3c392ba8a7a068051d
Kseniya <pursecookie@yandex.ru>
2023-01-29 23:05:59 +0300

    Правки по первому ревью внесены

--- src/tracker/service/InMemoryHistoryManager.java
+++ src/tracker/service/InMemoryHistoryManager.java
@@ -2,21 +2,21 @@ package tracker.service;
 
 import tracker.model.Task;
 
-import java.util.ArrayList;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    List<Task> history = new ArrayList<>();
+    List<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
         if (history.size() >= 10) {
             history.remove(0);
-            history.add(task);
-        } else {
-            history.add(task);
         }
+        history.add(task);
     }
+
 
     @Override
     public List<Task> getHistory() {
